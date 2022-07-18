af95ef08cc627dc198f1ebca83db854dfc3f4a3f
Sameer Khan <febsameer@gmail.com>
2022-07-18 14:03:02 +0530

    Added base64 file  support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import io
 import time
+import base64
 from io import BytesIO
 import urllib.request
 from flask import Flask, request, redirect, jsonify
@@ -17,8 +18,36 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/parse', methods=['POST'])
-def upload_file():
+@app.route('/parse/base64', methods=['POST'])
+def parseBase64():
+    filename = request.json['filename']
+    if allowed_file(filename):
+        try:
+            filename = str(time.time_ns()) + secure_filename(filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filedata = base64.b64decode(request.json['filedata'])
+
+            with open(filepath, 'wb') as f:
+                f.write(filedata)
+                
+            data = ResumeParser(filepath).get_extracted_data()
+            
+            os.remove(filepath)
+            
+            resp = jsonify({'message' : 'File successfully uploaded', 'data' : data})
+            resp.status_code = 201
+            return resp
+        except:
+            resp = jsonify({'message' : 'Server Error'})
+            resp.status_code = 503
+            return 
+    else:
+        resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
+
+@app.route('/parse/file', methods=['POST'])
+def parse():
     # check if the post request has the file part
     if 'file' not in request.files:
         resp = jsonify({'message' : 'No file part in the request'})
